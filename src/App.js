ebad2b878e8e851884338feec15188146d8562f6
tarcisiusdaniel <tarcih@uw.edu>
2019-07-08 17:29:54 +0700

    copy manually

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        numOfClicks: 0
+    };
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+
+  handleIncrement(){
+      this.setState({
+        numOfClicks: this.state.numOfClicks + 1
+      });
+  }
+
+  handleDecrement(){
+      this.setState({
+        numOfClicks: this.state.numOfClicks - 1
+      });
+  }
+
+  render(){
+    return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, my name is <br/>{this.props.name}</h1>
+        <div id = "changeNumber">
+          <button onClick = {this.handleIncrement}>Increment</button>
+          <button onClick = {this.handleDecrement}>Decrement</button>
+        </div>
+        <p>Number of the button has been clicked: {this.state.numOfClicks}</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+    </div>
+  );
+  }
+}
+export default App;
